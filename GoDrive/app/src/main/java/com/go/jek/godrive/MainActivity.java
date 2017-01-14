1d9cc3c8fdbd97cca1883ec4705ffff529dab659
Sabareeshnath <sabareeshnahtkp@ymail.com>
2017-01-14 13:42:18 +0530

    Inital Commit
    GoDrive Andoid  App Base Code

---
+++ GoDrive/app/src/main/java/com/go/jek/godrive/MainActivity.java
@@ -0,0 +1,13 @@
+package com.go.jek.godrive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
